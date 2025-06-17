83d740c482ff924873f7884621fb53dee8f99630
ericamendez <ecmendez25@gmail.com>
2025-06-17 18:49:46 -0400

    more

--- packages/notes-intermediate-ts-v2/src/10-type-registry-revisited/index.ts
+++ packages/notes-intermediate-ts-v2/src/10-type-registry-revisited/index.ts
@@ -1,10 +1,10 @@
 import { fetchRecord, fetchRecords } from './lib/registry'
 
 async function main() {
-    // const myBook = await fetchRecord("book", "book_123")
-    // const myMagazine = await fetchRecord("magazine", "magazine_123")
-    // const myBookList = await fetchRecords("book", ["book_123"])
-    // const myMagazineList = await fetchRecords("magazine", ["magazine_123"])
+    const myBook = await fetchRecord("book", "book_123")
+    const myMagazine = await fetchRecord("magazine", "magazine_123")
+    const myBookList = await fetchRecords("book", ["book_123"])
+    const myMagazineList = await fetchRecords("magazine", ["magazine_123"])
 
     // //@ts-expect-error
     // fetchRecord("book", "booooook_123")
